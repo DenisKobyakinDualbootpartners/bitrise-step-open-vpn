bc0f81bcdf4a614bae4b3f5cf4fd7eb6fb6d9121
Denis Kobyakin <denis.kobyakin@dualbootpartners.com>
2021-10-07 11:05:39 +0400

    add pass

--- step.sh
+++ step.sh
@@ -44,9 +44,9 @@ EOF
     echo ${ca_crt} | base64 -D -o ca.crt > /dev/null 2>&1
     echo ${client_crt} | base64 -D -o client.crt > /dev/null 2>&1
     echo ${client_key} | base64 -D -o client.key > /dev/null 2>&1
-    echo ${password} > ./openvpn/auth.conf
+    echo ${password} > ./openvpn/user.ovpn-pass
 
-    sudo openvpn --client --dev tun --proto ${proto} --remote ${host} ${port} --daemon --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key > /dev/null 2>&1 &
+    sudo openvpn --client --dev tun --proto ${proto} --remote ${host} ${port} --askpass user.ovpn-pass --daemon --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key > /dev/null 2>&1 &
 
     sleep 5
 

78f45bb551e2dba991b53c8addb6c46688b77943
Denis Kobyakin <denis.kobyakin@dualbootpartners.com>
2021-10-07 11:24:41 +0400

    increase sleep delay

--- step.sh
+++ step.sh
@@ -48,7 +48,7 @@ EOF
 
     sudo openvpn --client --dev tun --proto ${proto} --remote ${host} ${port} --askpass user.ovpn-pass --status /run/openvpn.status 10 --daemon --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key > /dev/null 2>&1 &
 
-    sleep 5
+    sleep 60
 
     if ifconfig -l | grep utun0 > /dev/null
     then
